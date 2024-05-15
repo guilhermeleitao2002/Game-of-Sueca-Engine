62ae12654ad8eecdcb77d3c23e0a8e931b46a7b7
Guilherme Leitão <guilhermeleitao0202@tecnico.ulisboa.pt>
2024-05-15 15:11:44 +0100

    feat: add different strategies to parser helper

--- sueca.py
+++ sueca.py
@@ -12,8 +12,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Sueca game simulator')
 
     parser.add_argument('-o', '--output', type=str, required=True, help='Output file to save the game log')
-    parser.add_argument('-t1', '--team_1', type=str, required=True, help='Strategy for team 1')
-    parser.add_argument('-t2', '--team_2', type=str, required=True, help='Strategy for team 2')
+    parser.add_argument('-t1', '--team_1', type=str, required=True, help='Strategy for team 1: random, maxpointswon, maxroundswon, cooperative, greedy, predictor')
+    parser.add_argument('-t2', '--team_2', type=str, required=True, help='Strategy for team 2: random, maxpointswon, maxroundswon, cooperative, greedy, predictor')
     parser.add_argument('-n', '--num_games', type=int, default=1, help='Number of games to simulate')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Print the game information as it unfolds')
     parser.add_argument('-m', '--mode', type=str, default='auto', help='Mode of the game: auto (machine vs machine) or human (machine vs user)')
